8ff2758408c93bb933d0eee28b99426bb3959e27
EduardoPerez04 <90478811+EduardoPerez04@users.noreply.github.com>
2024-04-29 14:03:29 -0500

    changes
    changes

--- logic.py
+++ logic.py
@@ -1,16 +1,44 @@
 from PyQt6.QtWidgets import *
 from calculator import *
 
+
 class Logic(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+
+        #numbers
+        self.push_one.clicked.connect(self.num_clicked)
+        self.push_two.clicked.connect(self.num_clicked)
+        self.push_three.clicked.connect(self.num_clicked)
+        self.push_four.clicked.connect(self.num_clicked)
+        self.push_five.clicked.connect(self.num_clicked)
+        self.push_six.clicked.connect(self.num_clicked)
+        self.push_seven.clicked.connect(self.num_clicked)
+        self.push_eight.clicked.connect(self.num_clicked)
+        self.push_nine.clicked.connect(self.num_clicked)
+
+        #operators
+        self.push_divide.clicked.connect(self.calculate)
+        self.push_multiply.clicked.connect(self.calculate)
+        self.push_minus.clicked.connect(self.calculate)
+        self.push_add.clicked.connect(self.calculate)
+
         self.push_submit.clicked.connect(self.submit)
         self.push_clear.clicked.connect(self.clear_input)
-        self.ans_label.setText("")
 
-    def submit(self):
-        pass
+        self.current_input = []
+
+    def num_clicked(self):
+        self.current_input.append(float(self.sender().text()))
+        self.ans_label.setText("".join(self.current_input))
+
+    def calculate(self):
+        
+
+
+
 
     def clear_input(self):
         pass

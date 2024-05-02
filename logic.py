c8fd54b32042ba419a9a5c8836551ed4b0a1783d
Jordan Zhang <jordanzhang@unomaha.edu>
2024-05-01 21:15:06 -0500

    Changed code

--- logic.py
+++ logic.py
@@ -3,11 +3,19 @@ from gui import *
 
 
 class Logic(QMainWindow, Ui_MainWindow):
+    """
+    A class that connects the calculator UI to code
+    """
+
     def __init__(self):
+        """
+        A method that sets up the calculator UI
+        and connects the buttons to code
+        """
         super().__init__()
         self.setupUi(self)
 
-        #numbers
+        # numbers
         self.push_one.clicked.connect(self.num_clicked)
         self.push_two.clicked.connect(self.num_clicked)
         self.push_three.clicked.connect(self.num_clicked)
@@ -19,7 +27,7 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.push_nine.clicked.connect(self.num_clicked)
         self.push_decimal.clicked.connect(self.num_clicked)
 
-        #operators
+        # operators
         self.push_divide.clicked.connect(self.calculate)
         self.push_multiply.clicked.connect(self.calculate)
         self.push_minus.clicked.connect(self.calculate)
@@ -32,6 +40,9 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.total_input = []
 
     def num_clicked(self):
+        """
+        A method that reads input when the user clicks a number
+        """
         button = self.sender()
         number = button.text()
 
@@ -45,11 +56,16 @@ class Logic(QMainWindow, Ui_MainWindow):
         else:
             self.total_input.append(int(number))
             self.current_input.append(int(number))
+            print('current', self.current_input)
+            print('total', self.total_input)
 
         ans = "".join(map(str, self.total_input))
         self.ans_label.setText(ans)
 
     def calculate(self):
+        """
+        A method that reads input when the user clicks an operation
+        """
         button = self.sender()
         operation = button.text()
         self.current_input = [operation]
@@ -61,9 +77,15 @@ class Logic(QMainWindow, Ui_MainWindow):
                 self.ans_label.setText(ans)
 
     def submit(self):
+        """
+        A method that calculators an answer when the user clicks the equal button
+        """
         pass
 
     def clear_input(self):
+        """
+        A method that clears the current input on the calculator screen
+        """
         operators = ['×', '−', '+', '÷']
         has_operator = any(op in self.total_input for op in operators)
 
